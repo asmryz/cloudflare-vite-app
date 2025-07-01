c0f0a1f8e33f94dd79effa14a007a969fcf7faf6
Asim Riaz <asim.riaz@live.com>
2025-07-02 01:00:54 +0500

    Updates

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -3,81 +3,142 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+type Recap = { rid: number, course: string };
+type GradeAverage = {grade: string, total: number, per: number};
+
+// The App component fetches data from the server and displays it in a structured way
+// It allows users to select a year, semester, batch, and course recap
+// The selected parameters are displayed in JSON format for debugging purposes
 function App() {
-    const [years, setYears] = useState<{ year: string }[]>([]);
-    const [semesters, setSemesters] = useState<{ semester: string }[]>([]);
-    const [batches, setBatches] = useState<{ class: string }[]>([]);
-    const [params, setParams] = useState({ year: "", semester: "", batch: "" });
+	const [years, setYears] = useState<{ year: string }[]>([]);
+	const [semesters, setSemesters] = useState<{ semester: string }[]>([]);
+	const [batches, setBatches] = useState<{ class: string }[]>([]);
+	const [recaps, setRecaps] = useState<Recap[]>([]);
+    const [grades, setGrades] = useState<GradeAverage[]>([])
+	const [params, setParams] = useState<{ year: string; semester: string; batch: string; course: Recap | null }>({ year: "", semester: "", batch: "", course: null });
 
-    useEffect(() => {
-        fetch("/years/")
-            .then((res) => res.json() as Promise<{ year: string }[]>)
-            .then((data) => setYears(data));
-    }, []);
-    useEffect(() => {
-        if (params.year.length > 0) {
-            fetch(`/semesters/${params.year}`)
-                .then((res) => res.json() as Promise<{ semester: string }[]>)
-                .then((data) => setSemesters(data));
-        }
-    }, [params.year]);
+	useEffect(() => {
+		fetch("/years/")
+			.then((res) => res.json() as Promise<{ year: string }[]>)
+			.then((data) => setYears(data));
+	}, []);
+	useEffect(() => {
+		if (params.year.length > 0) {
+			fetch(`/semesters/${params.year}`)
+				.then((res) => res.json() as Promise<{ semester: string }[]>)
+				.then((data) => setSemesters(data));
+		}
+	}, [params.year]);
 
-    useEffect(() => {
-        if (params.semester.length > 0) {
-            fetch(`/batch/${params.year}/${params.semester}`)
-                .then((res) => res.json() as Promise<{ class: string }[]>)
-                .then((data) => setBatches(data));
-        }
-    }, [params.semester]);
-    
-    return (
-        <>
-            <h4>Years</h4>
-            <div style={{ display: "flex", gap: "1rem" }}>
-                {years.map((year) => (
-                    <div
-                        key={year.year}
-                        style={{ cursor: "pointer", backgroundColor: params.year === year.year ? "#42fd84" : "", padding: "0 5px" }}
-                        onClick={() => setParams({ ...params, year: year.year })}
-                    >
-                        {year.year}
-                    </div>
-                ))}
-            </div>
-            {semesters.length > 0 && (
-                <>
-                    <h4>Semesters</h4>
-                    <div style={{ display: "flex", gap: "1rem" }}>
-                        {semesters.map((semester) => (
-                            <div
-                                key={semester.semester}
-                                style={{ cursor: "pointer", backgroundColor: params.semester === semester.semester ? "#42fd84" : "", padding: "0 5px" }}
-                                onClick={() => setParams({ ...params, semester: semester.semester })}
-                            >
-                                {semester.semester}
-                            </div>
-                        ))}
-                    </div>
-                </>
-            )}
-            {batches.length > 0 && (
-                <>
-                    <h4>Batches</h4>
-                    <div style={{ display: "flex", gap: "1rem", flexWrap: "wrap" }}>
-                        {batches.map((batch) => (
-                            <div
-                                key={batch.class}
-                                style={{ cursor: "pointer", backgroundColor: params.batch === batch.class ? "#42fd84" : "", padding: "0 5px" }}
-                                onClick={() => setParams({ ...params, batch: batch.class })}
-                            >
-                                {batch.class}
-                            </div>
-                        ))}
-                    </div>
-                </>
-            )}
-        </>
-    );
+	useEffect(() => {
+		if (params.semester.length > 0) {
+			fetch(`/batch/${params.year}/${params.semester}`)
+				.then((res) => res.json() as Promise<{ class: string }[]>)
+				.then((data) => setBatches(data));
+		}
+	}, [params.semester]);
+	useEffect(() => {
+		if (params.batch.length > 0) {
+			fetch(`/batch/${params.year}/${params.semester}/${params.batch}`)
+				.then((res) => res.json() as Promise<Recap[]>)
+				.then((data) => setRecaps(data));
+		}
+	}, [params.batch]);
+	useEffect(() => {
+		if (params.course !== null) {
+			fetch(`/recap/${params.course.rid}`)
+				.then((res) => res.json() as Promise<GradeAverage[]>)
+				.then((data) => setGrades(data));
+		}
+	}, [params.course]);
+
+	return (
+		<>
+			<h4>Years</h4>
+			<div style={{ display: "flex" }}>
+				{years.map((year) => (
+					<div
+						key={year.year}
+						style={{ cursor: "pointer", backgroundColor: params.year === year.year ? "#42fd84" : "", padding: "0 10px" }}
+						onClick={() => setParams({ ...params, year: year.year })}
+					>
+						{year.year}
+					</div>
+				))}
+			</div>
+			{semesters.length > 0 && (
+				<>
+					<h4>Semesters</h4>
+					<div style={{ display: "flex" }}>
+						{semesters.map((semester) => (
+							<div
+								key={semester.semester}
+								style={{ cursor: "pointer", backgroundColor: params.semester === semester.semester ? "#42fd84" : "", padding: "0 10px" }}
+								onClick={() => setParams({ ...params, semester: semester.semester })}
+							>
+								{semester.semester}
+							</div>
+						))}
+					</div>
+				</>
+			)}
+			{batches.length > 0 && (
+				<>
+					<h4>Batches</h4>
+					<div style={{ display: "flex", flexWrap: "wrap" }}>
+						{batches.map((batch) => (
+							<div
+								key={batch.class}
+								style={{ cursor: "pointer", backgroundColor: params.batch === batch.class ? "#42fd84" : "", padding: "0 10px" }}
+								onClick={() => setParams({ ...params, batch: batch.class })}
+							>
+								{batch.class}
+							</div>
+						))}
+					</div>
+				</>
+			)}
+			{recaps.length > 0 && (
+				<>
+					<h4>Recaps</h4>
+					<div style={{ display: "flex", flexWrap: "wrap" }}>
+						{recaps.map((recap) => (
+							<div
+								style={{ cursor: "pointer", backgroundColor: params.course && params.course.rid === recap.rid ? "#42fd84" : "", padding: "0 10px" }}
+								onClick={() => setParams({ ...params, course: recap })}
+							>
+								{recap.course}
+							</div>
+						))}
+					</div>
+				</>
+			)}
+			{grades.length > 0 && (
+				<>
+					<h4>Grade Average</h4>
+					<table>
+                        <thead>
+                            <tr>
+                                <th>Grade</th>
+                                <th>Total</th>
+                                <th>Percentage(%)</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+						{grades.map((grade, index) => (
+                            <tr key={index}>
+                                <td>{grade.grade}</td>  
+                                <td>{grade.total}</td>  
+                                <td>{grade.per}</td>
+                            </tr>      
+						))}
+                        </tbody>
+					</table>
+				</>
+			)}
+            <pre style={{textAlign: 'left'}}>{JSON.stringify(params, null, 4)}</pre>
+		</>
+	);
 }
 
 export default App;
--- src/worker/routes/index.ts
+++ src/worker/routes/index.ts
@@ -1,40 +1,80 @@
-import { Hono } from 'hono';
+import { Hono } from "hono";
 const indexRoute = new Hono();
-import {sql} from '../db'; // Importing from db to ensure it's initialized
+import { sql } from "../db"; // Importing from db to ensure it's initialized
 
+indexRoute.get("/api/", async (c) => c.json({ name: "Cloudflare" }));
 
-indexRoute.get('/api/', async(c) => c.json({ name: 'Cloudflare' }));
-
-indexRoute.get('/years/', async(c) => {
-    try {
-        const years = await sql`SELECT DISTINCT(year) FROM recap ORDER BY Year;`;
-        console.log(years)
-        return c.json(years, 200);
-    } catch (err) {
-        return c.json({ error: (err as Error).message }, 500);
-    }
+indexRoute.get("/years/", async (c) => {
+	try {
+		const years = await sql`SELECT DISTINCT(year) FROM recap ORDER BY Year;`;
+		console.log(years);
+		return c.json(years, 200);
+	} catch (err) {
+		return c.json({ error: (err as Error).message }, 500);
+	}
 });
 
-indexRoute.get('/semesters/:year', async(c) => {
-    try {
-        const semesters = await sql`SELECT DISTINCT(semester) FROM recap WHERE year = ${c.req.param('year')} ORDER BY semester;`;
-        console.log(semesters)
-        return c.json(semesters, 200);
-    } catch (err) {
-        return c.json({ error: (err as Error).message }, 500);
-    }
+indexRoute.get("/semesters/:year", async (c) => {
+	try {
+		const semesters = await sql`SELECT DISTINCT(semester) FROM recap WHERE year = ${c.req.param("year")} ORDER BY semester;`;
+		console.log(semesters);
+		return c.json(semesters, 200);
+	} catch (err) {
+		return c.json({ error: (err as Error).message }, 500);
+	}
 });
 
-indexRoute.get('/batch/:year/:semester', async(c) => {
+indexRoute.get("/batch/:year/:semester", async (c) => {
+	try {
+		const batches = await sql`SELECT DISTINCT(Class) FROM recap WHERE Year = ${c.req.param("year")} AND Semester = ${c.req.param("semester")} ORDER BY Class;`;
+		batches.sort((a, b) => a.class.length - b.class.length);
+		console.log(batches);
+		return c.json(batches, 200);
+	} catch (err) {
+		return c.json({ error: (err as Error).message }, 500);
+	}
+});
 
-    try {
-        const batches = await sql`SELECT DISTINCT(Class) FROM recap WHERE Year = ${c.req.param('year')} AND Semester = ${c.req.param('semester')} ORDER BY Class;`;
-        batches.sort((a, b) => a.class.length - b.class.length);
-        console.log(batches)
-        return c.json(batches, 200);
-    } catch (err) {
-        return c.json({ error: (err as Error).message }, 500);
-    }
+indexRoute.get("/batch/:year/:semester/:batch", async (c) => {
+	try {
+		const {year, semester, batch } = c.req.param();
+		const recaps = await sql`SELECT r.rid, c.title || ' ('|| c.theory || ', ' || c.lab ||')' as course FROM recap r, course c WHERE r.cid = c.cid AND Year = ${year} AND Semester = ${semester} AND Class = ${batch};`;
+		console.log(recaps);
+		return c.json(recaps, 200);
+	} catch (err) {
+		return c.json({ error: (err as Error).message }, 500);
+	}
+});
+indexRoute.get("/recap/:rid", async (c) => {
+	try {
+        const { rid } = c.req.param();
+        const query = await sql`SELECT  grade, total, CAST(total AS FLOAT) * 100 / CAST(SUM AS FLOAT) Per
+                                FROM (
+                                    SELECT * , (
+                                        SELECT SUM(total)
+                                        FROM (
+                                            SELECT g.grade, COUNT(g.grade) total 
+                                            FROM cmarks m, grade g
+                                            WHERE rid = A.rid
+                                            AND hid = 246
+                                            AND ROUND(marks) BETWEEN g.start AND g.end
+                                            GROUP BY g.grade
+                                        ) B
+                                    ) SUM
+                                    FROM (
+                                    SELECT rid, g.grade, COUNT(g.grade) total 
+                                    FROM cmarks m, grade g
+                                    WHERE m.rid = ${rid}
+                                    AND hid = 246
+                                    AND ROUND(marks) BETWEEN g.start AND g.end
+                                    GROUP BY m.rid, g.grade
+                                    ) A
+                                ) C`;
+        console.log(query)
+		return c.json(query, 200);
+	} catch (err) {
+		return c.json({ error: (err as Error).message }, 500);
+	}
 });
 
 export default indexRoute;
